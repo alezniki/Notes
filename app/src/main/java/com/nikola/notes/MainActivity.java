28856b3b682084816af2b266bc85fde85ce32449
alezniki <alezniki@gmail.com>
2017-05-15 18:18:22 +0200

    Toolbar added

--- app/src/main/java/com/nikola/notes/MainActivity.java
+++ app/src/main/java/com/nikola/notes/MainActivity.java
@@ -1,13 +1,21 @@
 package com.nikola.notes;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Add Toolbar
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
