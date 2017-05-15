0f2e51d044afdb6d54c26b148fe8a35f93e6bb57
alezniki <alezniki@gmail.com>
2017-05-15 20:39:58 +0200

    Added toolbar menu action, changed action icons

--- app/src/main/java/com/nikola/notes/MainActivity.java
+++ app/src/main/java/com/nikola/notes/MainActivity.java
@@ -3,8 +3,11 @@ package com.nikola.notes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
 
@@ -13,14 +16,35 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Add Toolbar
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
 
+
     // Inflate Menu icons
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
-
-
+    // Respond to Actions
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        switch (item.getItemId()){
+            case R.id.search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.add:
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
