0de58d9d77bc00d4713e14e83a0975c7f137dfd1
alezniki <alezniki@gmail.com>
2017-05-15 20:04:40 +0200

    Added menu xml file and menu icon drawable

--- app/src/main/java/com/nikola/notes/MainActivity.java
+++ app/src/main/java/com/nikola/notes/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Toolbar toolbar;
 
@@ -13,9 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Add Toolbar
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
+
+    // Inflate Menu icons
+
+
+
 }
