fe8fe56b38557342c5aa333ddc1fe59a7d683363
alezniki <alezniki@gmail.com>
2017-05-17 19:09:13 +0200

    changed saveNote() function

--- app/src/main/java/com/nikola/notes/activities/SecondActivity.java
+++ app/src/main/java/com/nikola/notes/activities/SecondActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,10 +15,11 @@ import com.nikola.notes.R;
  * Created by Dzoni on 5/16/2017.
  */
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends MainActivity {
 
     Toolbar toolbar;
     EditText enterNote;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,13 +55,14 @@ public class SecondActivity extends AppCompatActivity {
     public void saveNote(){
         String note = String.valueOf(enterNote.getText());
 
-        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
-        intent.putExtra("note", note); // String name, Bundle value
-
         if (note.trim().isEmpty()) {
             Snackbar.make(findViewById(R.id.save),"Enter note dummy", Snackbar.LENGTH_SHORT).show();
         } else {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            intent.putExtra("note", note); // String name, Bundle value
+
             startActivity(intent);
         }
+
     }
 }
