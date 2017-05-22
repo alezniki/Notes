96ab35d9cb98914258f7123f9380fd5e5fee3fd8
alezniki <alezniki@gmail.com>
2017-05-23 01:11:11 +0200

    Added CardView widget

--- app/src/main/java/com/nikola/notes/adapters/NoteAdapter.java
+++ app/src/main/java/com/nikola/notes/adapters/NoteAdapter.java
@@ -1,28 +1,80 @@
 package com.nikola.notes.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.nikola.notes.R;
 
 /**
  * Created by nikola on 5/19/17.
  */
 
-public class NoteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+//
+//    //1. Create new views (invoked by the layout manager)
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//    //2. Replace the contents of a view (invoked by the layout manager)
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//
+//    }
+//
+//    //3. Return the size of your dataset (invoked by the layout manager)
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
 
-    //1. Create new views (invoked by the layout manager)
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
     }
-    //2. Replace the contents of a view (invoked by the layout manager)
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NoteAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    public NoteAdapter() {}
+
+     // Create new views (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public NoteAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.list_items, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
 
     }
 
-    //3. Return the size of your dataset (invoked by the layout manager)
+    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return 0;
+        //return mDataset.length;
     }
 }
